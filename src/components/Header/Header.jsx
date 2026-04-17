f4ac268150c1d12eefbb1df6c7ab92e7935a28b1
Quack923 <vanya.pich@gmail.com>
2026-04-18 05:51:26 +1000

    hw1 fix

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+function Header() {
+    return (
+        <header className="header">
+            <div className="container">
+                <div className="header__block">
+                    <div className="header__logo _show _light">
+                        <a href="" target="_self">
+                            <img src="images/logo.png" alt="logo" />
+                        </a>
+                    </div>
+                    <div className="header__logo _dark">
+                        <a href="" target="_self">
+                            <img src="images/logo_dark.png" alt="logo" />
+                        </a>
+                    </div>
+                    <nav className="header__nav">
+                        <button className="header__btn-main-new _hover01" id="btnMainNew">
+                            <a href="#popNewCard">Создать новую задачу</a>
+                        </button>
+                        <UserProfile />
+                    </nav>
+                </div>
+            </div>
+        </header>
+    );
+}
